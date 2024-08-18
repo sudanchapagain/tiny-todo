b6dde2e69f681b3f9a59cdd32294c930886e2dd2
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-18 12:26:14 +0545

    Add mark-done functionality

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -51,6 +51,10 @@ fun main(args: Array<String>) {
             markInProgress(args[1].toInt())
         }
 
+        "mark-done" -> {
+            markDone(args[1].toInt())
+        }
+
 
         else -> println("Unknown command: ${args[0]}")
     }
@@ -134,6 +138,25 @@ fun markInProgress(id: Int) {
 
         if (task != null) {
             task.status = "in-progress"
+            file.writeText(Json.encodeToString(tasks))
+            println("Task status updated successfully (ID: $id)")
+        } else {
+            println("Task not found: $id")
+        }
+    } catch (e: IOException) {
+        println("Error reading or writing to file: ${e.message}")
+    }
+}
+
+fun markDone(id: Int) {
+    try {
+        val jsonString = file.readText()
+        val tasks = Json.decodeFromString<MutableList<Task>>(jsonString)
+
+        val task = tasks.find { it.id == id }
+
+        if (task != null) {
+            task.status = "done"
             file.writeText(Json.encodeToString(tasks))
             println("Task status updated successfully (ID: $id)")
         } else {
