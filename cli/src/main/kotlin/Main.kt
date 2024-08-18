6f61b37a3c686fdf0fcf08afaf101c1d271d180a
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-18 11:08:40 +0545

    Add task add functionality

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 package src.main.kotlin
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.IOException
+
+@Serializable
+data class Task(val id: Int, var description: String, var status: String = "todo")
 
 fun main(args: Array<String>) {
     if (args.isEmpty() || args.size < 2) {
@@ -19,6 +25,14 @@ fun main(args: Array<String>) {
     }
 
     checkFile()
+
+    when (args[0]) {
+        "add" -> {
+            addTask(args[1])
+        }
+
+        else -> println("Unknown command: ${args[0]}")
+    }
 }
 
 fun checkFile() {
@@ -32,5 +46,27 @@ fun checkFile() {
         } catch (e: IOException) {
             println("Error creating or writing to file: ${e.message}")
         }
+    }
+}
+
+
+fun addTask(taskDescription: String) {
+    val filePath = "task_tracker_cli.json"
+    val file = File(filePath)
+
+    try {
+        val jsonString = file.readText()
+        // deserialize JSON string to instance of Muteable list where each object is named Task.
+        val tasks = Json.decodeFromString<MutableList<Task>>(jsonString)
+
+        val newId = (tasks.maxOfOrNull { it.id } ?: 0) + 1
+        val newTask = Task(id = newId, description = taskDescription)
+        tasks.add(newTask)
+
+        file.writeText(Json.encodeToString(tasks))
+
+        println("Task added successfully (ID: $newId)")
+    } catch (e: IOException) {
+        println("Error reading or writing to file: ${e.message}")
     }
 }
