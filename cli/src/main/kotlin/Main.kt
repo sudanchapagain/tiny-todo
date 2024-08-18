41c7e8318cfaa0b99021322e13e94919d92198cf
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-18 11:05:21 +0545

    print info

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 package src.main.kotlin
 
-fun main(){
-    println("Hello Kotlin")
+fun main(args: Array<String>) {
+    if (args.isEmpty() || args.size < 2) {
+        println("Usage: tasktrackercli <command> [<arguments>]")
+        println(
+            "commands:\n" +
+                    "\tadd (task),\n" +
+                    "\tupdate(id, task),\n" +
+                    "\tdelete(id),\n" +
+                    "\tmark-in-progress(id),\n" +
+                    "\tmark-done(id),\n" +
+                    "\tlist(done, todo, in-progress"
+        )
+        return
+    }
 }
